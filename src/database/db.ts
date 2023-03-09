11e9f52157ff758f15b5136a6a5e2dda34e52d3a
NathansStuff <n.odonnell@hotmail.com>
2023-03-10 05:36:06 +1100

    fixes

--- src/database/db.ts
+++ src/database/db.ts
@@ -4,14 +4,14 @@ import { MONGO_URI } from '../utils/config';
 
 export const connectDB = async (): Promise<void> => {
   if (MONGO_URI === '' || MONGO_URI === null) {
-    console.log('MONGO_URL is not defined in the env file'.red.underline.bold);
+    console.log('MONGO_URL is not defined in the env file');
     process.exit(1);
   }
   try {
     await mongoose.connect(MONGO_URI);
-    console.log('MongoDB connected'.blue.underline.bold);
+    console.log('MongoDB connected');
   } catch (err) {
-    console.log(err.message.red.underline.bold);
+    console.log(err.message);
     process.exit(1);
   }
 };
